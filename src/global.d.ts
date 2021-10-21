252981eddc8fb102954cd3cfa124290c98f0005e
yongkuan94 <yongkuan94@gmail.com>
2021-10-21 12:46:04 +0800

    Add global

---
+++ src/global.d.ts
@@ -0,0 +1,11 @@
+declare module '*.jpg' {
+	export default '' as string;
+}
+
+declare module '*.png' {
+	export default '' as string;
+}
+
+declare module '*.pdf' {
+	export default '' as string;
+}
